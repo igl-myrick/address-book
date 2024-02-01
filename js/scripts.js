c98d7e23307678edc243cbeefc1946f5da54932e
igl-myrick <igl.myrick@gmail.com>
2024-02-01 13:25:44 -0800

    add AddressBook constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+// AddressBook Business Logic
+function AddressBook() {
+  this.contacts = {};
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts[contact.firstName] = contact;
+};
+
+// Contacts Business Logic
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName,
   this.lastName = lastName,
