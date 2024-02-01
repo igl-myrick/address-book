0f2b67aedea11e03894b0890cf9ee929cc01c1a4
igl-myrick <igl.myrick@gmail.com>
2024-02-01 14:36:04 -0800

    add methods to AddressBook

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,33 @@
 // AddressBook Business Logic
 function AddressBook() {
   this.contacts = {};
+  this.currentID = 0;
 }
 
 AddressBook.prototype.addContact = function(contact) {
-  this.contacts[contact.firstName] = contact;
+  contact.id = this.assignID();
+  this.contacts[contact.id] = contact;
 };
+
+AddressBook.prototype.assignID = function() {
+  this.currentID += 1;
+  return this.currentID;
+};
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] !== undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+}
 
 // Contacts Business Logic
 function Contact(firstName, lastName, phoneNumber) {
