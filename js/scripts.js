ad10f6bacf00b30b54314e9243d122defc9f6659
igl-myrick <igl.myrick@gmail.com>
2024-02-16 13:55:47 -0800

    add dropdowns for address types

--- js/scripts.js
+++ js/scripts.js
@@ -31,20 +31,35 @@ AddressBook.prototype.deleteContact = function(id) {
 
 // Contacts Business Logic
 function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName,
-  this.lastName = lastName,
+  this.addresses = {};
+  this.firstName = firstName;
+  this.lastName = lastName;
   this.phoneNumber = phoneNumber;
+  this.addresses.emailAddress = [];
+  this.addresses.address = [];
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
-Contact.prototype.update = function(newFirstName, newLastName, newPhoneNumber) {
-  this.firstName = newFirstName,
-  this.lastName = newLastName,
+Contact.prototype.update = function(newFirstName, newLastName, newPhoneNumber, newEmailAddress, newAddress) {
+  this.firstName = newFirstName;
+  this.lastName = newLastName;
   this.phoneNumber = newPhoneNumber;
+  this.addresses.emailAddress = newEmailAddress;
+  this.addresses.address = newAddress;
 };
+
+Contact.prototype.addEmailAddress = function(emailAddress, emailType) {
+  this.addresses.emailAddress = emailAddress;
+  this.addresses.emailType = emailType;
+}
+
+Contact.prototype.addAddress = function(address, addressType) {
+  this.addresses.address = address;
+  this.addresses.addressType = addressType;
+}
 
 // UI Logic
 let addressBook = new AddressBook();
@@ -68,6 +83,10 @@ function displayContactDetails(event) {
   document.querySelector("#first-name").innerText = contact.firstName;
   document.querySelector("#last-name").innerText = contact.lastName;
   document.querySelector("#phone-number").innerText = contact.phoneNumber;
+  document.querySelector("#email-address").innerText = contact.addresses.emailAddress;
+  document.querySelector("#email-type").value = contact.addresses.emailType;
+  document.querySelector("#address").innerText = contact.addresses.address;
+  document.querySelector("#address-type").value = contact.addresses.addressType;
   document.querySelector("button.delete").setAttribute("id", contact.id);
   document.querySelector("div#contact-details").removeAttribute("class");
 }
@@ -77,12 +96,21 @@ function handleFormSubmission(event) {
   const inputtedFirstName = document.querySelector("input#new-first-name").value;
   const inputtedLastName = document.querySelector("input#new-last-name").value;
   const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
+  const inputtedEmailAddress = document.querySelector("input#new-email-address").value;
+  const inputtedEmailType = document.getElementById("email-type").value;
+  const inputtedAddress = document.querySelector("input#new-address").value;
+  const inputtedAddressType = document.getElementById("address-type").value;
   let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+  newContact.addEmailAddress(inputtedEmailAddress, inputtedEmailType);
+  newContact.addAddress(inputtedAddress, inputtedAddressType);
+  console.log(newContact)
   addressBook.addContact(newContact);
   listContacts(addressBook);
   document.querySelector("input#new-first-name").value = null;
   document.querySelector("input#new-last-name").value = null;
   document.querySelector("input#new-phone-number").value = null;
+  document.querySelector("input#new-email-address").value = null;
+  document.querySelector("input#new-address").value = null;
 }
 
 function handleDelete(event) {
